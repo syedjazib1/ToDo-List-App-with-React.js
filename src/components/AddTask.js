e0c4cb83cb7212bd46f2253bdbe51cfe851bfb15
Syed Jazib <113434930+syedjazib1@users.noreply.github.com>
2024-04-03 13:13:44 +0530

    styling changes

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,46 +1,47 @@
-import { useState } from 'react';
+import { useState } from "react";
 import Swal from "sweetalert2";
 const AddTask = ({ onSave }) => {
-    const [text, setText] = useState('');
-    const [day, setDay] = useState('');
-    const onSubmit = (e) => {
-        e.preventDefault();
-        if (!text && !day) {
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Fill in your task and date or close the form!'
-            })
-        } else if (!text && day) {
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Fill in your task!'
-            })
-        } else if (text && !day) {
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Fill in your date!'
-            })
-        } else {
-            onSave({ text, day });
-        }
-        setText('');
-        setDay('');
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!text && !day) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Fill in your task and date or close the form!",
+      });
+    } else if (!text && day) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Fill in your task!",
+      });
+    } else if (text && !day) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Fill in your date!",
+      });
+    } else {
+      onSave({ text, day });
     }
-    return (
-        <form className="add-form" onSubmit={onSubmit}>
-            <div className="form-control">
-                <label>Task</label>
-                <input type="text" placeholder="add task" value={text} onChange={(e) => setText(e.target.value)} />
-            </div>
-            <div className="form-control">
-                <label>Day & Time</label>
-                <input type="text" placeholder="add day & time" value={day} onChange={(e) => setDay(e.target.value)} />
-            </div>
-            <input type="submit" className="btn btn-block" value="Save Task" />
-        </form>
-    )
-}
+    setText("");
+    setDay("");
+  };
+  return (
+    <form className="add-form" onSubmit={onSubmit}>
+    <div className="form-control">
+        <label htmlFor="task" className="form-label">Task</label>
+        <input type="text" id="task" className="form-input" placeholder="Add task" value={text} onChange={(e) => setText(e.target.value)} />
+    </div>
+    <div className="form-control">
+        <label htmlFor="day-time" className="form-label">Day & Time</label>
+        <input type="text" id="day-time" className="form-input" placeholder="Add day & time" value={day} onChange={(e) => setDay(e.target.value)} />
+    </div>
+    <input type="submit" className="btn btn-submit" value="Save Task" />
+</form>
+
+  );
+};
 export default AddTask;
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = ({ showForm, changeTextAndColor }) => {
   return (
       <header className="header">
           <h2 className="app-header">ToDo List App</h2>
-          <Button onClick={showForm} color={changeTextAndColor ? 'red' : 'green'} text=     {changeTextAndColor ? 'Close' : 'Add'} />
+          <Button onClick={showForm} color={changeTextAndColor ? 'red' : '#0056b3'} text={changeTextAndColor ? 'Close' : 'Add'} />
         </header>
     )
 }
