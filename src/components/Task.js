a727a912a00254e4ffce214a931e7f0b7b4a5c3a
Syed Jazib <113434930+syedjazib1@users.noreply.github.com>
2024-04-04 23:27:34 +0530

    stlying addTask list

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,19 +3,20 @@ import "../index.css"
 const Task = ({ task, onDelete, onEdit }) => {
     return (
       <div>
-        <div className="task">
-          <div>
-            <p className="taskName">
-              <span className="textBold">Task Name:</span> {task.text}
-            </p>
-        <p className="taskDate"><span className="textBold">Date of Completion:</span>              {task.day}
-            </p>
-            </div>
-            <div>
-            <p><FaTimes onClick={() => onDelete(task.id)} className="delIcon" /></p>
-          <p><FaPencilAlt onClick={() => onEdit(task.id)} className="editIcon" /></p>
-          </div>
-        </div>
+        <div class="task">
+    <div>
+      <p class="taskName">
+        <span class="textBold">Task Name:</span> {task.text}
+      </p>
+      <p class="taskDate">
+        <span class="textBold">Date of Completion:</span> {task.day}
+      </p>
+    </div>
+    <div>
+      <p><FaTimes onClick={() => onDelete(task.id)} class="delIcon" /></p>
+      <p><FaPencilAlt onClick={() => onEdit(task.id)} class="editIcon" /></p>
+    </div>
+  </div>
       </div>
     )
 }
